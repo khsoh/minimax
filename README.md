85e262f84ff8179f68bdbfe5f08f27e5b7ad0937
K H Soh <hju37823@outlook.com>
2026-05-28 16:59:26 +0800

    Updated to support markdown preview and added README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# nvim-minimax
+
+## Introduction
+
+This is the Neovim setup based on ['nvim-minimax'](https://nvim-mini.org/MiniMax/).  Most of the
+library modules are from the ['mini.nvim'](https://nvim-mini.org/mini.nvim/) modules.
+
+## Setup
+
+The setup is simple - clone to `$HOME/.config/nvim` folder and then execute:
+
+```
+    nvim --headless "+lua vim.pack.update(nil, { force=true} )" "+MasonUpdate" "+MasonToolsUpdateSync" "+qa"
+```
+
+Alternatively, if you still wish to preserve the earlier nvim setup, then clone to `$HOME/.config/minimax` folder and then execute:
+
+```
+    NVIM_APPNAME=$HOME/.config/minimax nvim --headless "+lua vim.pack.update(nil, { force=true} )" "+MasonUpdate" "+MasonToolsUpdateSync" "+qa"
+```
+
---
+++ plugin/50_markdown.lua
@@ -0,0 +1,33 @@
+-- 1. Register the package blueprint with vim.pack globally
+vim.pack.add { Config.gh 'iamcco/markdown-preview.nvim' }
+
+-- 2. Bind everything to the filetype event to handle clean lazy loading
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    -- CRITICAL: Force Neovim to evaluate the plugin's internal scripts immediately
+    vim.cmd 'packadd markdown-preview.nvim'
+
+    -- Apply plugin configurations safely
+    vim.g.mkdp_auto_start = 0
+
+    -- Set the keymap locally strictly for this buffer
+    vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', {
+      desc = '[M]arkdown [P]review',
+      buffer = true, -- Attaches ONLY to active markdown files
+    })
+  end,
+})
+
+-- 3. Run the Node installer block once if missing
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  once = true,
+  callback = function()
+    if not vim.g.mkdp_node_installed then
+      vim.fn['mkdp#util#install']()
+      vim.g.mkdp_node_installed = true
+    end
+  end,
+})
+-- vim: ts=2 sts=2 sw=2 et
